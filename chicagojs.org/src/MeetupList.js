37253d9bcd6f0c5edae02ee25502ffa91ad1cc66
Rehabescapi <Jasonmlehmann@gmail.com>
2017-09-26 15:44:23 -0400

    objectified meetup list

--- chicagojs.org/src/MeetupList.js
+++ chicagojs.org/src/MeetupList.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function getMeetupList()
+{
+  return [{
+    url : 'https://www.meetup.com/Chicago-Nodejs/',
+    title: 'Chicago Node.js',
+    description:'A group to discuss Node.js, its frameworks, and best practices.',
+    nextMeetup:''
+  },
+  {
+    url : 'https://www.meetup.com/React-Chicago/',
+    title: 'React Chicago',
+    description:'This meetup is focused on bringing everyone together who have an interest in React, React Native, and it\'s ecosystem.',
+    nextMeetup:''
+  },
+  {
+    url : 'https://www.meetup.com/AngularJS-Chicago/',
+    title: 'AngularJS Chicago',
+    description:'AngularJS meetup group in Chicago, IL discussing how to leverage and evolve the \'Superheroic JavaScript MVW Framework\'.',
+    nextMeetup:''
+  }];
+}
+
+
+function RenderRow({foo, rep}) {
+var meetups =[]
+ meetups = getMeetupList();
+  return(
+      <div className="lists">
+        {meetups.map(({url , title, description},index) => (
+      <div key ={index}> <h3> <a href={url}> {title} </a></h3>
+            <p className='{title}'> {description} </p>
+      </div>
+      )
+        )}
+    </div>
+    )
+}
+
+
 class MeetupList extends Component {
   render() {
     return (
         <div className="Meetup-list">
           <h2>Current Chicago Area JavaScript Meetups</h2>
-          <h3><a href="https://www.meetup.com/js-chi/">Chicago JavaScript Meetup Group</a></h3>
-          <p>Meet other local JavaScript Developers to network, share knowledge, to review the state of the art, or to dream up new applications.</p>  
+         <RenderRow/>
 
-          <h3><a href="https://www.meetup.com/Chicago-Nodejs/">Chicago Node.js</a></h3>
-          <p>A group to discuss Node.js, its frameworks, and best practices.</p> 
 
-          <h3><a href="https://www.meetup.com/React-Chicago/">React Chicago</a></h3>
-          <p>This meetup is focused on bringing everyone together who have an interest in React, React Native, and it's ecosystem.</p>
-
-          <h3><a href="https://www.meetup.com/AngularJS-Chicago/">AngularJS Chicago</a></h3>
-          <p>AngularJS meetup group in Chicago, IL discussing how to leverage and evolve the 'Superheroic JavaScript MVW Framework'.</p>
         </div>
     );
   }
